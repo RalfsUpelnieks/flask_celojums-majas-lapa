a2a551e13f21789677dee6ff2881ff16a9933a1f
DP1RUpelnieks <>
2021-06-08 12:22:09 +0000

    Pievienots javascript

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,24 @@
-console.log("Hello!");
+document.addEventListener("DOMContentLoaded", () => {
+
+    const LOGIN_BUTTON = document.getElementById("login");
+    const LOGIN_POPUP = document.getElementById("log-in-panel");
+
+    isLoggedIn = false;
+
+    LOGIN_BUTTON.addEventListener("click", () => {
+        if(isLoggedIn === false){
+            let login = prompt("Ievadiet jūsu lietotājvārdu!");
+            let pass = prompt("Ievadiet jūsu paroli!");
+            if (login === "user" && pass === "user") {
+                alert("Ielogošanās veiksmīga!");
+                isLoggedIn = true;
+            }
+            else {
+                alert("Lietotājvārds vai parole ir nepareiza.")
+            }
+        }
+        else {
+            window.location.href = "/profils";
+        }
+    });
+});
