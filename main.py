47de67e1f56738741d8169f40588f8887bb1eb25
DP1GRebuks <>
2021-11-04 15:13:20 +0000

    New files added - Admin page and Jinja

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
-app = Flask('app')
+from flask import Flask, render_template, redirect
+from flask.helpers import url_for
 
+app = Flask('app')
 @app.route('/')
 def index():
   return render_template("index.html")
@@ -11,10 +12,23 @@ def celojumi():
 
 @app.route('/admin')
 def admin():
-  return render_template("admin.html")
+  return redirect(url_for("admin_agencies"))
 
 @app.route('/profils')
 def profils():
   return render_template("profile.html")
 
-app.run(host='0.0.0.0', port=8080)
+@app.route('/admin/countries')
+def admin_countries():
+  return render_template("templates/countries.html", num = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,0])
+
+@app.route('/admin/agencies')
+def admin_agencies():
+  return render_template("templates/agencies.html", num = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,0])
+
+@app.route('/admin/trips')
+def admin_trips():
+  return render_template("templates/trips.html", num = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,0])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
